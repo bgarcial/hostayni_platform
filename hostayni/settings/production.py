628b99811121a5a1813ec459d3969e1ac2b64c98
Bernardo Garcia <botibagl@gmail.com>
2017-09-26 11:37:28 -0500

    Update production settings file

--- hostayni/settings/production.py
+++ hostayni/settings/production.py
@@ -11,10 +11,10 @@ DEBUG = False
 # ------ *** -------------
 
 # Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['hostayni.herokuapp.com']
