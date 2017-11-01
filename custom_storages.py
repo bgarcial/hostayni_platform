0c716962445a9eaa1c5bd1d4c9208370f72ad469
Bernardo Garcia <botibagl@gmail.com>
2017-10-31 23:33:00 -0500

    Fixing contact us view to anonymous users

--- custom_storages.py
+++ custom_storages.py
@@ -2,6 +2,7 @@ from django.conf import settings
 # from storages.backends.s3boto3 import S3Boto3Storage
 from storages.backends.s3boto import S3BotoStorage
 
+
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
 
