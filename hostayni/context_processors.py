2f1584f63603993cac0774e9a0da3e14f4fdbc7e
Bernardo Garcia <botibagl@gmail.com>
2017-11-01 10:01:23 -0500

    Adding caption to images random

--- hostayni/context_processors.py
+++ hostayni/context_processors.py
@@ -2,14 +2,15 @@
 
 from random import choice
 
-images_people = ['https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/bolivia2.jpg','2',
-          'https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/bolivia3.jpg',
-          'https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/bolivia4.jpg',
-          'https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/bolivia-children.jpg',
-          'https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/ElZocalo.jpg',
-          'https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/israel.jpg',
-          ]
+images_people = [
+    ('https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/bolivia2.jpg', 'Caption 1'),
+    ('https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/bolivia3.jpg', 'Caption 2'),
+    ('https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/bolivia4.jpg', 'Caption 3'),
+    ('https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/bolivia-children.jpg', 'Caption 4'),
+    ('https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/ElZocalo.jpg', 'Caption 5'),
+    ('https://s3-sa-east-1.amazonaws.com/ihost-project/assets/img/israel.jpg', 'Caption 6',)
+]
 
 def images(request):
     # return {'frase': 'Niche ojos'}
-    return {'images': choice(images_people)}
+    return {'image': choice(images_people)}
