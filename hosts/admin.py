79aadcc8e16e39e312d393814efd6d0c0a2b91b7
Bernardo Garcia <botibagl@gmail.com>
2018-01-20 23:06:08 -0500

    Admin offers

---
+++ hosts/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from django.contrib import admin
+from .models import LodgingOffer, StudiesOffert, StudyOfferImage
+    #StudiesTypeOffered, StudiesOffertList, ,
+
+
+@admin.register(StudyOfferImage)
+class StudyOfferImageAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'study_offer', 'image']
+    # list_editable = ['order', 'active', 'featured']
+
+@admin.register(LodgingOffer)
+class LodgingOfferAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ad_title', 'is_taked', 'is_paid', 'room_type_offered', 'number_guest_room_type',
+                     'room_value', 'additional_description', 'slug')
+
+    list_editable = ('is_taked', 'is_paid')
+
+@admin.register(StudiesOffert)
+class StudiesOffertAdmin(admin.ModelAdmin):
+    list_display = ('id', 'is_taked', 'ad_title', 'slug', 'tag_list',)
+    list_editable = ('is_taked',)
+
+    def get_queryset(self, request):
+        return super(StudiesOffertAdmin, self).get_queryset(request).prefetch_related('knowledge_topics')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.knowledge_topics.all())
