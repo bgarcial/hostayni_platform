b822b922a4baedbc8f63cb5fe88a6d62b7c34acc
Bernardo Garcia <botibagl@gmail.com>
2018-02-09 02:00:28 -0500

    Removing debug tool bar from main urls.py

--- hostayni/urls.py
+++ hostayni/urls.py
@@ -129,11 +129,11 @@ if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
+'''
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
-
+'''
 
