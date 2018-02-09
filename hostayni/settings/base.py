3c12289073c2e6c4312543bd563662e07e5612e7
Bernardo Garcia <botibagl@gmail.com>
2018-02-09 03:00:21 -0500

    removing debug tool bar middleware

--- hostayni/settings/base.py
+++ hostayni/settings/base.py
@@ -86,7 +86,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'hostayni.urls'
