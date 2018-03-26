7217b55cf71238af2879730dd338487df5af7ca6
Bernardo García <botibagl@gmail.com>
2018-03-26 18:15:52 -0500

    Registering Entrepreneurship Offer to admin

--- entrepreneurship/admin.py
+++ entrepreneurship/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import EntrepreneurshipOffer
 
 # Register your models here.
+
+@admin.register(EntrepreneurshipOffer)
+class EntrepreneurshipOfferAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ad_title', 'is_taked', 'is_paid', 'offer_type', 'price',
+                     'country', 'city', 'date', 'url', 'contact_name')
+
+    list_editable = ('is_taked', 'is_paid')
