7f3152dd627b621cd487712d537cd67890423340
Bernardo García <botibagl@gmail.com>
2018-04-16 11:37:10 -0500

    Adding beta.hostayni.com to ALLOWED_HOST directive on production [heroku] settings

--- hostayni/settings/production.py
+++ hostayni/settings/production.py
@@ -27,7 +27,7 @@ DATABASES['default'].update(db_from_env)
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['hostayni.herokuapp.com', 'www.hostayni.com']
+ALLOWED_HOSTS = ['hostayni.herokuapp.com', 'www.hostayni.com', 'beta.hostayni.com']
 
 HAYSTACK_CONNECTIONS = {
     'default': {
