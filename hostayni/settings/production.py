190293632d67582580c851e60dc52952acefb550
Bernardo García <botibagl@gmail.com>
2018-04-16 16:26:48 -0500

    Setting heroku amazon hostname deployment

--- hostayni/settings/production.py
+++ hostayni/settings/production.py
@@ -11,7 +11,7 @@ DATABASES = {
         'NAME': get_env_variable('DATABASE_NAME'),
         'USER': get_env_variable('DATABASE_USER'),
         'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
-        'HOST': 'ec2-23-23-142-5.compute-1.amazonaws.com',
+        'HOST': 'ec2-54-243-28-109.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
